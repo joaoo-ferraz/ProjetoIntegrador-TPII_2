8b490e9a740d694607ed19c91d5affec8fb13acd
João Ferraz <joaophaulo03@gmail.com>
2026-09-21 21:36:10 -0300

    Criando AlunoDAO

---
+++ Aula 7/siga-dao/src/siga/AlunoDAO.java
@@ -0,0 +1,11 @@
+package siga;
+
+import java.util.List;
+
+public interface AlunoDAO {
+    void inserir(Aluno aluno);
+    Aluno buscarPorMatricula(String matricula);
+    List<Aluno> listarTodos();
+    void atualizar(Aluno aluno);
+    void remover(String matricula);
+}
